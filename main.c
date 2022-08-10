e3c971b24ab11d94075375cd28219b1a4db50b84
hbadejo <badejohakeem95@gmail.com>
2022-08-10 11:44:11 -0700

    Formatting main.c to betty requirement

--- main.c
+++ main.c
@@ -16,53 +16,53 @@
  */
 int main(int argc, char *argv[])
 {
-    int ret_val = 0, retn;
-    int *exe_ret = &retn;
-    char shell_prompt[15] = "haksam";
-    char *prompt = _strcat(shell_prompt, " $ "), *new_com = "\n";
+	int ret_val = 0, retn;
+	int *exe_ret = &retn;
+	char shell_prompt[15] = "haksam";
+	char *prompt = _strcat(shell_prompt, " $ "), *new_com = "\n";
 
-    name = argv[0];
-    hist = 1;
-    ll_aliases = NULL;
-    signal(SIGINT, sig_handler);
+	name = argv[0];
+	hist = 1;
+	ll_aliases = NULL;
+	signal(SIGINT, sig_handler);
 
-    *exe_ret = 0;
-    environ = _copyenv();
-    if (!environ)
-        exit(-100);
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
 
-    if (argc != 1)
-    {
-        ret_val = file_stdin(argv[1], exe_ret);
-        free_env();
-        free_alias_list(ll_aliases);
-        return (*exe_ret);
-    }
+	if (argc != 1)
+	{
+		ret_val = file_stdin(argv[1], exe_ret);
+		free_env();
+		free_alias_list(ll_aliases);
+		return (*exe_ret);
+	}
 
-    if (!isatty(STDIN_FILENO))
-    {
-        while (ret_val != END_OF_FILE && ret_val != EXIT)
-            ret_val = handle_argv(exe_ret);
-        free_env();
-        free_alias_list(ll_aliases);
-        return (*exe_ret);
-    }
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret_val != END_OF_FILE && ret_val != EXIT)
+			ret_val = handle_argv(exe_ret);
+		free_env();
+		free_alias_list(ll_aliases);
+		return (*exe_ret);
+	}
 
-    while (1)
-    {
-        write(STDOUT_FILENO, prompt, 15);
-        ret_val = handle_argv(exe_ret);
-        if (ret_val == END_OF_FILE || ret_val == EXIT)
-        {
-            if (ret_val == END_OF_FILE)
-                write(STDOUT_FILENO, new_com, 1);
-            free_env();
-            free_alias_list(ll_aliases);
-            exit(*exe_ret);
-        }
-    }
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 15);
+		ret_val = handle_argv(exe_ret);
+		if (ret_val == END_OF_FILE || ret_val == EXIT)
+		{
+			if (ret_val == END_OF_FILE)
+			write(STDOUT_FILENO, new_com, 1);
+			free_env();
+			free_alias_list(ll_aliases);
+			exit(*exe_ret);
+		}
+	}
 
-    free_env();
-    free_alias_list(ll_aliases);
-    return (*exe_ret);
+	free_env();
+	free_alias_list(ll_aliases);
+	return (*exe_ret);
 }
